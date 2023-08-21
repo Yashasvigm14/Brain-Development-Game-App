19bed081b177a64ed19c428b3f729c0d01047a58
Yashasvi GM <132649643+Yashasvigm14@users.noreply.github.com>
2023-08-21 20:01:31 +0530

    Add files via upload

---
+++ brain_game/build/generated/source/buildConfig/debug/ism3053/fgcubrainage/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ism3053.fgcubrainage;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ism3053.fgcubrainage";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
